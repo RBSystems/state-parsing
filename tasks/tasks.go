ac6896aacaa9a3bdabc8ae0951ab22da56a2f2ed
Joseph Blodgett <Joseph.blodgett@gmail.com>
2018-06-13 09:15:37 -0600

    adding general alert clearing meta data

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -4,6 +4,7 @@ import (
 	"github.com/byuoitav/state-parsing/alerts/heartbeat"
 	"github.com/byuoitav/state-parsing/tasks/names"
 	"github.com/byuoitav/state-parsing/tasks/task"
+	"github.com/byuoitav/state-parsing/update/generalalert"
 	"github.com/byuoitav/state-parsing/update/roomupdate"
 )
 
@@ -16,6 +17,7 @@ func init() {
 	tasks[names.ROOM_UPDATE] = task.NewTask(&roomupdate.RoomUpdater{})
 	tasks[names.LOST_HEARTBEAT] = task.NewTask(&heartbeat.LostHeartbeatAlertFactory{})
 	tasks[names.HEARTBEAT_RESTORED] = task.NewTask(&heartbeat.RestoredHeartbeatAlertFactory{})
+	tasks[names.GENERAL_ALERT_UPDATE] = task.NewTask(&generalalert.GeneralAlertUpdater{})
 }
 
 func GetTask(name string) (task.Task, bool) {
--- update/generalalert/queries.go
+++ update/generalalert/queries.go
@@ -30,3 +30,33 @@ var GeneralAlertQuery = &common.ElkQuery{
   "size": 100
 }`,
 }
+
+type GeneralAlertQueryResponse struct {
+	Hits struct {
+		Total int `json:"total"`
+		Hits  []struct {
+			Index  string  `json:"_index"`
+			Type   string  `json:"_type"`
+			ID     string  `json:"_id"`
+			Score  float64 `json:"_score"`
+			Source struct {
+				Alerting              bool   `json:"alerting"`
+				Hostname              string `json:"hostname"`
+				LastHeartbeat         string `json:"last-heartbeat"`
+				EnableNotifications   string `json:"enable-notifications"`
+				LastStateRecieved     string `json:"last-state-recieved"`
+				SuppressNotifications string `json:"suppress-notifications"`
+				Control               string `json:"control"`
+				ViewDashboard         string `json:"view-dashboard"`
+				Room                  string `json:"room"`
+				Alerts                struct {
+					LostHeartbeat struct {
+						AlertSent string `json:"alert-sent"`
+						Message   string `json:"message"`
+						Alerting  bool   `json:"alerting"`
+					} `json:"lost-heartbeat"`
+				} `json:"alerts"`
+			} `json:"_source"`
+		} `json:"hits"`
+	} `json:"hits"`
+}
