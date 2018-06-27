dae5ef36c413a112aa10c4b414307fe6d6e72cee
Daniel Randall <dgrandall@me.com>
2018-06-27 17:07:03 -0600

    fix script execution to include timeout

--- jobs/scripts.go
+++ jobs/scripts.go
@@ -1,8 +1,10 @@
 package jobs
 
 import (
+	"context"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/byuoitav/common/log"
 	"github.com/byuoitav/state-parsing/actions"
@@ -12,14 +14,18 @@ type ScriptJob struct {
 	Path string
 }
 
-func (j *ScriptJob) Run(context interface{}) []actions.ActionPayload {
+func (j *ScriptJob) Run(data interface{}) []actions.ActionPayload {
 	if len(j.Path) == 0 {
 		log.L.Errorf("path for a script job wasn't set. can't run this job.")
 		return []actions.ActionPayload{}
 	}
 
+	// add context for timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	// build the cmd
-	cmd := exec.Command(j.Path)
+	cmd := exec.CommandContext(ctx, j.Path)
 	cmd.Stdout = os.Stdout // TODO these should match wherever the logger is going
 	cmd.Stderr = os.Stderr
 
@@ -28,6 +34,7 @@ func (j *ScriptJob) Run(context interface{}) []actions.ActionPayload {
 	err := cmd.Run()
 	if err != nil {
 		log.L.Warnf("error executing script %s: %s", j.Path, err)
+		return []actions.ActionPayload{}
 	}
 
 	log.L.Infof("Script %s ran successfuly.")
